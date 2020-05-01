c3f2a77957ee9bab72426bb446f17f419f0edf32
AlfredoArandan <alfredo.arandan@gmail.com>
2020-05-01 01:08:49 -0400

    Primer test sobre como era clonar proyecto

---
+++ codes.py
@@ -0,0 +1,14 @@
+# Import packages
+
+import numpy as np
+import pandas as pd
+
+N = 100
+df = pd.DataFrame({
+    'A': pd.date_range(start='2016-01-01',periods=N,freq='D'),
+    'x': np.linspace(0,stop=N-1,num=N),
+    'y': np.random.rand(N),
+    'C': np.random.choice(['Low','Medium','High'],N).tolist(),
+    'D': np.random.normal(100, 10, size=(N)).tolist()
+})
+df.head()
